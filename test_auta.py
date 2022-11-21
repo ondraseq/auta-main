38dc19c96aada69df27118abd6c8ce1bfd2030f9
Ondřej Mateásko <mateasko@arcig.cz>
2022-11-21 14:35:42 +0100

    pridan test_equal

--- test_auta.py
+++ test_auta.py
@@ -6,3 +6,8 @@ def test_atributy():
     assert a.model == "Fabia"
     assert a.objem == "2L"
     assert a.barva == "Modra"
+
+def test_equal():
+    a1 = Auta("Skoda", "Fabia", "2L", "Modra")
+    a2 = Auta("Skoda", "Fabia", "2L", "Modra")
+    assert a1 == a2
